d17b4d96fca0a2826c3307fc876acc9529ebf238
hmarkoet <henningmarkoetter@gmx.de>
2023-07-13 17:07:32 +0200

    grid as function

--- BAMline_on_the_fly_CT-reco/on_the_fly_CT_hdf5_panel.py
+++ BAMline_on_the_fly_CT-reco/on_the_fly_CT_hdf5_panel.py
@@ -8,9 +8,9 @@ import math
 import time
 import os
 import csv
-import cv2
+import cv2                                      #to install package with pycharm by finding "opencv-python"
 from scipy.ndimage.filters import gaussian_filter, median_filter
-import pvaccess as pva      #to install search for "pvapy"
+import pvaccess as pva                          #to install package with pycharm search for "pvapy"
 
 # On-the-fly-CT Reco
 version =  "Version 2023.07.04 a"
@@ -18,7 +18,7 @@ version =  "Version 2023.07.04 a"
 #Install ImageJ-PlugIn: EPICS AreaDetector NTNDA-Viewer, look for the channel specified here under channel_name, consider multiple users on servers!!!
 channel_name = 'BAMline:CTReco'
 #standard_path = "C:/temp/HDF5-Reading/220130_1734_604_J1_anode_half_cell_in-situ_Z30_Y5430_15000eV_1p44um_500ms/" # '/mnt/raid/CT/2022/'
-standard_path = r'C:\delete\continuos_on-the-fly\220130_1757_607_J1_anode_half_cell_in-situ_Z30_Y5400_15000eV_3p61um_100ms'
+standard_path = r'C:/delete/reg_data/18_230606_2044_AlTi_F_Ref_tomo___Z25_Y6500_25000eV_10x_400ms'
 
 Ui_on_the_fly_Window, Q_on_the_fly_Window = loadUiType('on_the_fly_CT_reco_hdf_dock_widget.ui')  # connect to the GUI for the program
 
@@ -290,6 +290,26 @@ class On_the_fly_CT_tester(Ui_on_the_fly_Window, Q_on_the_fly_Window):
             print('pixel size not found')
         self.pixel_size.setEnabled(True)
 
+        #get and prefill energy
+        if '/entry/instrument/NDAttributes/DMM_Energy' in f:
+            self.energy_proxy = f['/entry/instrument/NDAttributes/DMM_Energy']
+            print('self.energy_proxy', self.energy_proxy[0], self.energy_proxy[-1])
+            self.doubleSpinBox_Energy_2.setValue(self.energy_proxy[0])
+        else:
+            self.doubleSpinBox_Energy_2.setValue(1)
+            print('Energy size not found')
+        self.doubleSpinBox_Energy_2.setEnabled(True)
+
+        #get and prefill distance
+        if '/entry/instrument/NDAttributes/CT-Kamera-Z' in f:
+            self.distance_proxy = f['/entry/instrument/NDAttributes/CT-Kamera-Z']
+            print('self.distance_proxy', self.distance_proxy[0], self.distance_proxy[-1])
+            self.doubleSpinBox_distance_2.setValue((self.distance_proxy[0] + 25)/10)
+        else:
+            self.doubleSpinBox_distance_2.setValue(1)
+            print('Distance not found')
+        self.doubleSpinBox_distance_2.setEnabled(True)
+
         self.load()
 
 
@@ -455,40 +475,14 @@ class On_the_fly_CT_tester(Ui_on_the_fly_Window, Q_on_the_fly_Window):
         self.slice_size = slices.shape[1]
         slices = tomopy.circ_mask(slices, axis=0, ratio=1.0)
 
-        slice = slices[0,:,:]   #reduce dimensions from 3 to 2
+        self.slice = slices[0,:,:]   #reduce dimensions from 3 to 2
 
         if self.checkBox_ruler.isChecked():
-            f = h5py.File(self.path_klick, 'r')
-            if '/entry/instrument/NDAttributes/CT_Piezo_X45' in f:
-                self.piezo_45_proxy = f['/entry/instrument/NDAttributes/CT_Piezo_X45']
-                print('self.piezo_45_proxy: ', self.piezo_45_proxy[0], self.piezo_45_proxy[-1], round(1000*self.piezo_45_proxy[-1]))
-                self.piezo_135_proxy = f['/entry/instrument/NDAttributes/CT_Piezo_Y45']
-                print('self.piezo_135_proxy: ', self.piezo_135_proxy[0], self.piezo_135_proxy[-1], round(1000*self.piezo_135_proxy[-1]))
-
-
-            #add ruler +X
-            for r in range(round(slice.shape[1]/(2*self.pixel_size.value())), round(slice.shape[1]/self.pixel_size.value()), round(self.spinBox_ruler_grid.value()/self.pixel_size.value())):
-                cv2.line(slice, (round(r*self.pixel_size.value()), 0), (round(r*self.pixel_size.value()), slice.shape[0]), (65535, 65535, 65535), 4)
-                cv2.putText(slice, str(round(1000*self.piezo_45_proxy[-1]/5)*5 + round(r*self.pixel_size.value()/5)*5 - round(slice.shape[1]/10)*5), (round(r*self.pixel_size.value()) + 20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.750, (65535, 65535, 65535), thickness=2)
-
-            #add ruler -X
-            for r in range(round(slice.shape[1]/(2*self.pixel_size.value())), 0, -round(self.spinBox_ruler_grid.value()/self.pixel_size.value())):
-                cv2.line(slice, (round(r*self.pixel_size.value()), 0), (round(r*self.pixel_size.value()), slice.shape[0]), (65535, 65535, 65535), 4)
-                cv2.putText(slice, str(round(1000*self.piezo_45_proxy[-1]/5)*5 + round(r*self.pixel_size.value()/5)*5 - round(slice.shape[1]/10)*5), (round(r*self.pixel_size.value()) + 20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.750, (65535, 65535, 65535), thickness=2)
-
-            #add ruler +Y
-            for r in range(round(slice.shape[1]/(2*self.pixel_size.value())), round(slice.shape[1]/self.pixel_size.value()), round(self.spinBox_ruler_grid.value()/self.pixel_size.value())):
-                cv2.line(slice, (0, round(r*self.pixel_size.value())), (slice.shape[1], round(r*self.pixel_size.value())), (65535, 65535, 65535), 4)
-                cv2.putText(slice, str(round(1000*self.piezo_135_proxy[-1]/5)*5 + round(r*self.pixel_size.value()/5)*5 - round(slice.shape[1]/10)*5), ( 20, round(r*self.pixel_size.value()) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.750, (65535, 65535, 65535), thickness=2)
-
-            #add ruler -Y
-            for r in range(round(slice.shape[1]/(2*self.pixel_size.value())), 0, -round(self.spinBox_ruler_grid.value()/self.pixel_size.value())):
-                cv2.line(slice, (0, round(r*self.pixel_size.value())), (slice.shape[1], round(r*self.pixel_size.value())), (65535, 65535, 65535), 4)
-                cv2.putText(slice, str(round(1000*self.piezo_135_proxy[-1]/5)*5 + round(r*self.pixel_size.value()/5)*5 - round(slice.shape[1]/10)*5), ( 20, round(r*self.pixel_size.value()) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.750, (65535, 65535, 65535), thickness=2)
+            self.add_ruler()
 
 
         #crop before sending to ImageJ      spinBox_left
-        original_reconstruction = slice[self.spinBox_top.value():-self.spinBox_bottom.value()-1, self.spinBox_left.value():-self.spinBox_right.value()-1]
+        original_reconstruction = self.slice[self.spinBox_top.value():-self.spinBox_bottom.value()-1, self.spinBox_left.value():-self.spinBox_right.value()-1]
         print('original_reconstruction.shape', original_reconstruction.shape)
 
         # set image dimensions only for the first time or when scan-type was changed
@@ -531,7 +525,61 @@ class On_the_fly_CT_tester(Ui_on_the_fly_Window, Q_on_the_fly_Window):
         self.pushReconstruct.setText('Test')
         self.pushReconstruct_all.setText('Reconstruct\n Volume')
 
+    def add_ruler(self):
+        f = h5py.File(self.path_klick, 'r')
+        if '/entry/instrument/NDAttributes/CT_Piezo_X45' in f:
+            self.piezo_45_proxy = f['/entry/instrument/NDAttributes/CT_Piezo_X45']
+            print('self.piezo_45_proxy: ', self.piezo_45_proxy[0], self.piezo_45_proxy[-1],
+                  round(1000 * self.piezo_45_proxy[-1]))
+            self.piezo_135_proxy = f['/entry/instrument/NDAttributes/CT_Piezo_Y45']
+            print('self.piezo_135_proxy: ', self.piezo_135_proxy[0], self.piezo_135_proxy[-1],
+                  round(1000 * self.piezo_135_proxy[-1]))
+        else:
+            self.piezo_45_proxy = (0,0)
+            self.piezo_135_proxy = (0,0)
 
+
+        # add ruler +X
+        for r in range(round(self.slice.shape[1] / (2 * self.pixel_size.value())),
+                       round(self.slice.shape[1] / self.pixel_size.value()),
+                       round(self.spinBox_ruler_grid.value() / self.pixel_size.value())):
+            cv2.line(self.slice, (round(r * self.pixel_size.value()), 0),
+                     (round(r * self.pixel_size.value()), self.slice.shape[0]), (65535, 65535, 65535), 4)
+            cv2.putText(self.slice, str(round(1000 * self.piezo_45_proxy[-1] / 5) * 5 + round(
+                r * self.pixel_size.value() / 5) * 5 - round(self.slice.shape[1] / 10) * 5),
+                        (round(r * self.pixel_size.value()) + 20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.750,
+                        (65535, 65535, 65535), thickness=2)
+
+        # add ruler -X
+        for r in range(round(self.slice.shape[1] / (2 * self.pixel_size.value())), 0,
+                       -round(self.spinBox_ruler_grid.value() / self.pixel_size.value())):
+            cv2.line(self.slice, (round(r * self.pixel_size.value()), 0),
+                     (round(r * self.pixel_size.value()), self.slice.shape[0]), (65535, 65535, 65535), 4)
+            cv2.putText(self.slice, str(round(1000 * self.piezo_45_proxy[-1] / 5) * 5 + round(
+                r * self.pixel_size.value() / 5) * 5 - round(self.slice.shape[1] / 10) * 5),
+                        (round(r * self.pixel_size.value()) + 20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.750,
+                        (65535, 65535, 65535), thickness=2)
+
+        # add ruler +Y
+        for r in range(round(self.slice.shape[1] / (2 * self.pixel_size.value())),
+                       round(self.slice.shape[1] / self.pixel_size.value()),
+                       round(self.spinBox_ruler_grid.value() / self.pixel_size.value())):
+            cv2.line(self.slice, (0, round(r * self.pixel_size.value())),
+                     (self.slice.shape[1], round(r * self.pixel_size.value())), (65535, 65535, 65535), 4)
+            cv2.putText(self.slice, str(round(1000 * self.piezo_135_proxy[-1] / 5) * 5 + round(
+                r * self.pixel_size.value() / 5) * 5 - round(self.slice.shape[1] / 10) * 5),
+                        (20, round(r * self.pixel_size.value()) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.750,
+                        (65535, 65535, 65535), thickness=2)
+
+        # add ruler -Y
+        for r in range(round(self.slice.shape[1] / (2 * self.pixel_size.value())), 0,
+                       -round(self.spinBox_ruler_grid.value() / self.pixel_size.value())):
+            cv2.line(self.slice, (0, round(r * self.pixel_size.value())),
+                     (self.slice.shape[1], round(r * self.pixel_size.value())), (65535, 65535, 65535), 4)
+            cv2.putText(self.slice, str(round(1000 * self.piezo_135_proxy[-1] / 5) * 5 + round(
+                r * self.pixel_size.value() / 5) * 5 - round(self.slice.shape[1] / 10) * 5),
+                        (20, round(r * self.pixel_size.value()) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.750,
+                        (65535, 65535, 65535), thickness=2)
 
     def reconstruct_all(self):
         #grey out the buttons while program is busy
